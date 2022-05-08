ba63e0aead35c74fc13f1cdd07244a8b3e308c30
Ricardo de Lima Oliveira <ricardo.lima.oliveira@uol.com.br>
2022-05-08 16:55:21 -0300

    Init TableView

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -14,10 +14,17 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
+    var messages: [Message] = [
+        Message(sender: "1@2.com", body: "Hey!"),
+        Message(sender: "a@b.com", body: "Hello!"),
+        Message(sender: "1@2.com", body: "What's up?")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        title = "⚡️FlashChat"
+        tableView.dataSource = self
+        title = K.appName
         navigationItem.hidesBackButton = true
         
     }
@@ -35,5 +42,20 @@ class ChatViewController: UIViewController {
             print("Error signing out: %@", signOutError)
         }
         
+    }
+}
+
+extension ChatViewController: UITableViewDataSource{
+ 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell =  tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath)
+        
+        cell.textLabel?.text = "This a cell!"
+        
+        return cell
     }
 }
--- Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -23,7 +23,7 @@ class RegisterViewController: UIViewController {
                     print(e.localizedDescription)
                 }else{
                     //Navigate to the ChartViewController
-                    self.performSegue(withIdentifier: "RegisterToChat", sender: self)
+                    self.performSegue(withIdentifier: K.registerSegue, sender: self)
                 }
             }
         }
